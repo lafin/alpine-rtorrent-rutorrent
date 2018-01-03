a3afa04c53ff443fde51198329bc28154db2cea4
Kuvakin Sergey <kuvakin@gmail.com>
2018-01-04 02:36:02 +0300

    update

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ for ARCH in "${ARCHS[@]}"
 do
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
     cp Dockerfile.stable origin.stable
-    sed -ie "s/FROM alpine:3.6/FROM multiarch\/alpine:${ARCH}\-v3.6/" Dockerfile.stable
+    sed -ie "s/FROM alpine:3.7/FROM multiarch\/alpine:${ARCH}\-v3.7/" Dockerfile.stable
     docker build -t lafin/alpine-rtorrent-rutorrent:${ARCH}-stable -f Dockerfile.stable .
     mv origin.stable Dockerfile.stable
     rm Dockerfile.stablee
 
     docker run --rm --privileged multiarch/qemu-user-static:register --reset
     cp Dockerfile.latest origin.latest
-    sed -ie "s/FROM alpine:3.6/FROM multiarch\/alpine:${ARCH}\-v3.6/" Dockerfile.latest
+    sed -ie "s/FROM alpine:3.7/FROM multiarch\/alpine:${ARCH}\-v3.7/" Dockerfile.latest
     docker build -t lafin/alpine-rtorrent-rutorrent:${ARCH}-latest -f Dockerfile.latest .
     mv origin.latest Dockerfile.latest
     rm Dockerfile.lateste
